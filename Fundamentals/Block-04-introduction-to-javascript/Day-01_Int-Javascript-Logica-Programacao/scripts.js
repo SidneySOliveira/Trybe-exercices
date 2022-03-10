b868c16433a9a0de50043573ea45423b250b8f8d
SidneyOliveira12 <sidneydsdoliveira@gmail.com>
2022-03-10 16:10:19 -0300

    adicionando arquivos do bloco 03

---
+++ Fundamentals/Block-04-introduction-to-javascript/Day-01_Int-Javascript-Logica-Programacao/scripts.js
@@ -0,0 +1,9 @@
+let myName= "Sidney";
+let birthCity= "Macapá";
+let birthYear= 32;
+birthYear= 2030;
+birthCity= "Brasil";
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
---
+++ Fundamentals/Block-04-introduction-to-javascript/Day-03_Logic-Programming-and-algorithms/exercicio.js
@@ -0,0 +1,94 @@
+// 1. Faça um algoritmo que calcule a soma de 50 a 100 usando a estrutura “for” e retorne no formato:
+// A soma de 50 a 100 é: X.
+console.log("Exercício 01");
+let soma = 0
+for (let i=50; i<=100; i+=1){
+    soma += i
+}
+console.log("Soma de 50 a 100 é ", soma);
+// 2. Crie um algoritmo que conte quantos números do intervalo entre 1 e 125 são divisíveis por 3.
+// Caso a quantidade seja igual a 50, exiba uma mensagem secreta a sua escolha.
+console.log(" ");
+console.log("Exercício 02");
+let divisiveis = 0
+
+for (let i=1; i<=125; i+=1){
+    if (i%3 === 0){
+        divisiveis += 1
+    }
+}
+if (divisiveis === 50){
+    console.log("São divisiveis por 3 ", divisiveis)
+} else {
+    console.log("É IGUAL A 50")
+}
+
+// 3. Crie um algoritmo que simula o jogo “pedra, papel e tesoura” levando em consideração que são apenas duas pessoas jogando e imprima o resultado no formato:
+// “Jogador 1 vence” ou “Empate” ou “Jogador 2 vence”.
+console.log(" ");
+console.log("Exercício 03");
+let jogador01 = Math.floor(Math.random()*3);
+let jogador02 = Math.floor(Math.random()*3);
+
+if (jogador01 === 1){ jogador01 = "Pedra"}
+else if (jogador01 ===2){ jogador01 = "Papel"}
+else {jogador01 = "Tesoura"}
+if (jogador02 === 1){ jogador02 = "Pedra"}
+else if (jogador02 ===2){ jogador02 = "Papel"}
+else {jogador02 = "Tesoura"}
+console.log("Jogador 01: ", jogador01);
+console.log("Jogador 02: ", jogador02);
+
+if(jogador01 === jogador02){
+    "Empate"
+} else if (jogador01 === "Pedra" && jogador02 === "Tesoura") {
+    console.log("Jogador 01 Venceu!")
+}
+else if (jogador01 === "Tesoura" && jogador02 === "Papel") {
+    console.log("Jogador 01 Venceu!")
+}
+else if (jogador01 === "Papel" && jogador02 === "Pedra") {
+    console.log("Jogador 01 Venceu!")
+}
+else {console.log("Jogador 02 Venceu!")}
+
+// 4. Desenvolva um algoritmo que verifica se a pessoa é maior ou menor de idade.
+// Imprima no console seguindo o exemplo: “A pessoa é maior de idade”.
+console.log(" ");
+console.log("Exercício 04");
+
+let idade = Math.floor(Math.random()*130);
+console.log("Idade: ", idade);
+
+if (idade < 18){
+    console.log("A pessoa é MENOR de idade!")
+} else {
+    console.log("A pessoa é MAIOR de idade")
+}
+
+// 5. Crie um algoritmo que recebe a idade de Marcella, Carlos e Camila e imprima quem é a mais nova no formato:
+// “Pessoa” é a mais nova.
+console.log(" ");
+console.log("Exercício 05");
+
+let Marcella = Math.floor(Math.random()*130);
+let Carlos = Math.floor(Math.random()*130);
+let Camila = Math.floor(Math.random()*130);
+let menorIdade = 1000;
+let idades = [Marcella, Carlos, Camila];
+
+for (let i=0; i<idades.length; i+=1){
+    if (menorIdade > idades[i]) {
+        menorIdade = idades[i];
+    }
+}
+console.log(idades);
+if (menorIdade === Marcella){
+    console.log("Marcella é a mais nova com ", menorIdade);
+}
+else if (menorIdade === Carlos) {
+    console.log("Carlos é o mais novo com ", menorIdade);
+}
+else if (menorIdade === Camila) {
+    console.log("Camila é a mais nova com ", menorIdade);
+}
