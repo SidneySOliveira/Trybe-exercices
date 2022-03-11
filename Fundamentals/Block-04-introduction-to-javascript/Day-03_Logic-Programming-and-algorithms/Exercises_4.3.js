4d629778cbf2216d53beb16eadc128fa5b9c08d3
SidneyOliveira12 <sidneydsdoliveira@gmail.com>
2022-03-11 14:32:55 -0300

    Iniciado os exercicios bonus do modulo Fundamentos, Bloco 04 dia 3

--- Fundamentals/Block-04-introduction-to-javascript/Day-03_Logic-Programming-and-algorithms/Exercises_4.3.js
+++ Fundamentals/Block-04-introduction-to-javascript/Day-03_Logic-Programming-and-algorithms/Exercises_4.3.js
@@ -1,36 +1,56 @@
-// Exercise 01 - Crie um algoritmo que retorne o fatorial de 10
+// // Exercise 01 - Crie um algoritmo que retorne o fatorial de 10
 
-let number = 1;
-for (i=1; i<=10; i += 1){
-    number = number * i
-};
-console.log("Excercise 01 - O fatorial de 10 é ", number);
-
-
-//Exercise 02 - Desenvolva um algoritmo que é capaz de inverter uma palavra
-
-let word = "trybe";
-let newword = '';
-for (i=0; i<word.length;  i += 1){
-    a = word.length -1 - i
-    newword = newword.concat(word[a])
-};
-
-console.log("Excercise 02 - A palavra invertida é ", newword);
+// let number = 1;
+// for (i=1; i<=10; i += 1){
+//     number = number * i
+// };
+// console.log("Excercise 01 - O fatorial de 10 é ", number);
 
 
-// Exercise 03 - algoritmo que retorne a maior e menor palavra de uma array
+// //Exercise 02 - Desenvolva um algoritmo que é capaz de inverter uma palavra
 
-let array = ['java', 'javascript', 'python', 'html', 'css'];
-menorPalavra = "1000000000000000000000000000000000000000000000000";
-maiorPalavra = ""
-for (i=0; i<array.length; i += 1){
-    if (menorPalavra.length > array[i].length) {
-        menorPalavra = array[i]
-    };
-    if (maiorPalavra.length < array[i].length) {
-        maiorPalavra = array[i]
+// let word = "trybe";
+// let newword = '';
+// for (i=0; i<word.length;  i += 1){
+//     a = word.length -1 - i
+//     newword = newword.concat(word[a])
+// };
+
+// console.log("Excercise 02 - A palavra invertida é ", newword);
+
+
+// // Exercise 03 - algoritmo que retorne a maior e menor palavra de uma array
+
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// menorPalavra = "1000000000000000000000000000000000000000000000000";
+// maiorPalavra = ""
+// for (i=0; i<array.length; i += 1){
+//     if (menorPalavra.length > array[i].length) {
+//         menorPalavra = array[i]
+//     };
+//     if (maiorPalavra.length < array[i].length) {
+//         maiorPalavra = array[i]
+//     }
+// }
+
+// console.log("Excercise 03 - A maior palavra é ", maiorPalavra, " e a menor é ", menorPalavra);
+
+// Exercise 04 - Escreva um algoritmo que retorne o maior número primo entre 0 e 50.
+let limInferior = 1;
+let limSuperior = 50;
+let divisiveis = 0
+let primos = [];
+let maiorPrimo = "";
+
+for (i=limInferior; i<=limSuperior; i +=1){
+    let divisiveis = 0
+    for (j=limInferior; j<=i; j+=1){
+        if (i%j === 0) {
+            divisiveis += 1
+        }
     }
-}
-
-console.log("Excercise 03 - A maior palavra é ", maiorPalavra, " e a menor é ", menorPalavra);
+    if (divisiveis === 2){
+        primos.push(i)
+    }
+};
+console.log("O maior numero primo é", primos[primos.length-1]);
