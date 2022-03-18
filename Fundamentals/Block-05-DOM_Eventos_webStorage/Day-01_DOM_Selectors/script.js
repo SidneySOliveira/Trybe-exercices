e068aee61161cfe6c2b71494ac8c200f51132dec
SidneySOliveira <sidneydsdoliveira@gmail.com>
2022-03-18 15:32:58 -0300

    finalizando exercícios dia 2

--- Fundamentals/Block-05-DOM_Eventos_webStorage/Day-01_DOM_Selectors/script.js
+++ Fundamentals/Block-05-DOM_Eventos_webStorage/Day-01_DOM_Selectors/script.js
@@ -1,16 +1,16 @@
-/*
 // Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
 document.getElementsByTagName("p")[0].innerText = "Trabalhando em um grande projeto";
+
 // Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
 document.getElementsByClassName('main-content')[0].style.backgroundColor = 'rgb(76,164,109)';
+
 // Crie uma função que mude a cor do quadrado vermelho para branco.
 document.getElementsByTagName('section')[0].style.backgroundColor = 'rgb(255,255,255)';
+
 // Crie uma função que corrija o texto da tag <h1>.
 document.getElementsByTagName('h1')[0].innerText = 'Exercício 5.1 - JavaEscript';
+
 // Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
-*/
-
-
 function Maiuscula (){
     let pMaiuscula = document.getElementsByTagName('p');
         for(i = 0; i < pMaiuscula.length; i += 1){
