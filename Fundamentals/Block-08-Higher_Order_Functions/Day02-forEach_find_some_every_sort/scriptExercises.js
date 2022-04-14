ea0c84df5a105c6e58698eb5ab8926fe9fbda2e4
SidneySOliveira <sidneydsdoliveira@gmail.com>
2022-04-13 21:02:18 -0300

    Exercicios 02 e 03 dia 02 concluidos

--- Fundamentals/Block-08-Higher_Order_Functions/Day02-forEach_find_some_every_sort/scriptExercises.js
+++ Fundamentals/Block-08-Higher_Order_Functions/Day02-forEach_find_some_every_sort/scriptExercises.js
@@ -1,7 +1,28 @@
 const {books} = require('./dados')
-
+/*
+// 1 - Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947.
 function authorBornIn1947(library, yearSearch) {
     return library.find((year) => year.author.birthYear === yearSearch)
   }
 
 console.log(authorBornIn1947(books, 1947))
+
+// 2 - Retorne o nome do livro de menor nome.
+function smallerName(book) {
+    let nameBook = book[0].name;
+    book.forEach(element => { 
+        element.name.length < nameBook.length ? nameBook = element.name : nameBook = nameBook;   
+    });
+    return nameBook;
+  }
+
+  console.log(smallerName(books));
+//   console.log(books[0].name.length);
+*/
+// 3 - Encontre o primeiro livro cujo nome possui 26 caracteres.
+  
+  function getNamedBook(books, nameLength) {
+      return books.find((nome) => nome.name.length = nameLength)
+  }
+
+  console.log(getNamedBook(books, 26));
